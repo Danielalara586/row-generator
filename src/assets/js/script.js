96bbf1d9302ee26db0f1e4c75c3020a07bc8e99e
Daniela Lara <dalo@gft.com>
2022-02-06 15:22:37 -0600

    Finish function to create row

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,5 +1,29 @@
-const createRow = function(){
-    const row = document.createElement('tr');
+let counter = 1;
+
+const createRow = ($event) => {
+    // Var declaration
+    const $row = document.createElement('tr');
+    const $id = document.createElement('th');
+    const $name = document.createElement('td');
+    const $email = document.createElement('td');
+    const $age = document.createElement('td');
+
+    $id.textContent = counter;
+    $name.textContent = `name ${counter}`;
+    $email.textContent = `name${counter}@example.com`;
+    $age.textContent = counter * 3;
+
+    $row.appendChild($id);
+    $row.appendChild($name);
+    $row.appendChild($email);
+    $row.appendChild($age);
+
+    const $body = document.querySelector('table tbody');
+    $body.appendChild($row);
+    counter += 1;
+    console.log(counter);
+
 }
 
-const addButton = document.getElementById('add');
+const $addButton = document.getElementById('add');
+$addButton.addEventListener('click', createRow);
